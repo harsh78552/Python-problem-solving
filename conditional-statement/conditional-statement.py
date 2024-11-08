0ea63ddaeb4006b19cabddd7c7dd6718f9a8d569
harsh78552 <ht728350@gmail.com>
2024-11-08 20:08:07 +0530

    update ccode

--- conditional-statement/conditional-statement.py
+++ conditional-statement/conditional-statement.py
@@ -1,6 +1,15 @@
-def print_():
-    return 'hello'
+# Find the largest number
+def find_largest(input_, input__, input___):
+    if input__ < input_ > input___:
+        return f'{input_} is greater...'
+    elif input_ < input__ > input___:
+        return f'{input__} is greater...'
+    else:
+        return f'{input___} is greater...'
 
 
-X = print_()
-print(X)
+Input_ = int(input("Enter first number:"))
+Input__ = int(input("Enter first number:"))
+Input___ = int(input("Enter first number:"))
+Output = find_largest(Input_, Input__, Input___)
+print(Output)
