708e5270d177ad02d99c71dec433f2137569ed4e
harsh78552 <ht728350@gmail.com>
2024-11-11 09:51:16 +0530

    new code added

--- String/string-question-practice.py
+++ String/string-question-practice.py
@@ -1,5 +1,7 @@
 # 1.Count Vowels in a String
-def count_vowels(str_, vowel_list_):
+""""""
+
+"""def count_vowels(str_, vowel_list_):
     dict_ = {}
 
     for i in vowel_list_:
@@ -14,4 +16,23 @@ def count_vowels(str_, vowel_list_):
 Input_string = input("Enter string: ").replace(" ", "").lower()
 list_ = ['a', 'e', 'i', 'o', 'u']
 Output = count_vowels(Input_string, list_)
+print(Output)"""
+
+
+# 2.Find the First Non-Repeating Character
+def count_first_non_repeating_character(str_):
+    dict_ = {}
+    for i in str_:
+        if i not in dict_:
+            dict_[i] = 1
+        else:
+            dict_[i] += 1
+    for key, value in dict_.items():
+        if value == 1:
+            return key
+    return None
+
+
+Input = "Swiss".lower()
+Output = count_first_non_repeating_character(Input)
 print(Output)
