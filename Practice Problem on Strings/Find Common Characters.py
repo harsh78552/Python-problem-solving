5cc2d7d4818313a08c60cbf5fa9b0d5a83760b3a
harsh78552 <ht728350@gmail.com>
2024-12-06 08:13:41 +0530

    code updated

---
+++ Practice Problem on Strings/Find Common Characters.py
@@ -0,0 +1,8 @@
+def word_list(list_):
+    dict_ = {}
+
+
+
+Input_List = ['bella', 'label', 'roller']
+Output = word_list(Input_List)
+print(Output)
--- Practice Problem on Strings/Longest Palindrome.py
+++ Practice Problem on Strings/Longest Palindrome.py
@@ -1,26 +1,37 @@
-def Palindrome_Length(string_):
+def make_palindrome(string_):
     dict_ = {}
-    result = 0
-    odd_count_present = False
+    if len(string_) < 1:
+        return string_
+    else:
+        for i in string_:
+            if i not in dict_:
+                dict_[i] = 1
+            else:
+                dict_[i] += 1
 
-    for char in string_:
-        if char not in dict_:
-            dict_[char] = 1
-        elif char in string_:
-            dict_[char] += 1
+        for key, value in dict_.items():
+            if value % 2 != 0:
+                dict_[key] = value - 1
 
-    for key, value in dict_.items():
-        if value % 2 != 0:
-            dict_[key] = value - 1
-            result += dict_[key]
-            odd_count_present = True
-        elif value % 2 == 0:
-            result += dict_[key]
-    if odd_count_present:
-        return result + 1
-    return result
+        initialise_list = [None] * (sum(list(dict_.values())) + 1)
+        count_ = 0
+        count1 = len(initialise_list) - 1
+
+        for key1, value1 in dict_.items():
+            if value1 == 0:
+                initialise_list[len(initialise_list) // 2] = key1
+            else:
+                for k in range(value1 // 2):
+                    if count_ <= count1:
+                        initialise_list[count_] = key1
+                        initialise_list[count1] = key1
+                        count_ += 1
+                        count1 -= 1
+    string__ = ''.join(initialise_list)
+    return f"Palindrome of input string is: {string__}"
 
 
-Input_String = 'aabbcc'
-Output = Palindrome_Length(Input_String)
+Input_String = 'abbcdccac'
+sorted_str = ''.join(sorted(Input_String))
+Output = make_palindrome(sorted_str)
 print(Output)
