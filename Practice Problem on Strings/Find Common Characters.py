d4abe929c0488105c69cc5e58bc6248f894f2156
harsh78552 <ht728350@gmail.com>
2024-12-07 16:07:27 +0530

    new code added

--- Practice Problem on Strings/Find Common Characters.py
+++ Practice Problem on Strings/Find Common Characters.py
@@ -1,8 +1,29 @@
-def word_list(list_):
+def common_character(list_):
     dict_ = {}
+    for char in list_[0]:
+        if char not in dict_:
+            dict_[char] = 1
+        else:
+            dict_[char] += 1
+    for word in list_[1:]:
+        dict__ = {}
+        for char in word:
+            if char not in dict__:
+                dict__[char] = 1
+            else:
+                dict__[char] += 1
+        for char1 in list(dict_.keys()):
+            if char1 in dict__:
+                dict_[char1] = min(dict_[char1], dict__[char1])
+            else:
+                del dict_[char1]
+    result = []
+    for key, value in dict_.items():
+        for j in range(value):
+            result.append(key)
+    return result
 
 
-
-Input_List = ['bella', 'label', 'roller']
-Output = word_list(Input_List)
+Input_list = ['bella', 'label', 'roller']
+Output = common_character(Input_list)
 print(Output)
